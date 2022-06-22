ef6a4b4d66a5dda9cbc76ecda45d0c19c016f8ec
adriel.santos <adriel200010@hotmail.com>
2022-06-22 14:29:13 -0300

    Coloquei o Prisma e configurei com o BD

--- server/controllers/agencyController.js
+++ server/controllers/agencyController.js
@@ -2,7 +2,22 @@ const Sequelize = require("sequelize");
 
 require("dotenv").config();
 
- 
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const allBanks = await prisma.banco.findMany();
+  console.log(allBanks)
+}
+
+main()
+  .catch((e) => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
 
 //View Agencies
 exports.view = (req,res) => {
