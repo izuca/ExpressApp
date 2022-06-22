b20b9b34f51bac4fee96e58d41370cf11d9c06a0
adriel.santos <adriel200010@hotmail.com>
2022-06-22 14:01:05 -0300

    Tirando o Sequelize

--- server/controllers/bankController.js
+++ server/controllers/bankController.js
@@ -1,28 +1,5 @@
-const Sequelize = require("sequelize");
-const tabela = require("../../models/bancos");
 
-require("dotenv").config();
-
- //Database Connection
- const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect:"postgres",
-    port: 5455
-});
-
-//Check Database
+//View Banks
 exports.view = (req,res) => {
-    tabela.findAll().then((posts) => {
-        res.render("banks",{id: 2, nome: "xablesco",numero: "002"});
-
-    })
-
-    sequelize.authenticate().then(() =>{
-        console.log("Banco Conectado")
-    }).catch((erro) =>{
-        console.log(`Falha ao se conectar: ${erro}`);
-    })
-
-    
-    
+    res.render("banks");
 }
